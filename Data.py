013db2caf0cdb50093c8423ebaf5ba10a9045e16
JeffZhang314 <zhang.jj.jeff@gmail.com>
2024-07-01 10:26:41 -0700

    finish one hot except for specific cases

--- Data.py
+++ Data.py
@@ -92,6 +92,18 @@ class Data():
                                     224, 225, 226, 227, 228, 229, 231, 236, 237, 238, 239, 240, 241, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 270, 271, 272, 273, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 313, 314, 315, 316, 317, 318, 319, 320, 321, 4426, 4428, 333, 334, 335, 4432, 4433, 338, 339, 340, 4437, 342, 4439, 4440, 4441, 4442, 4443, 4445, 4446, 4447, 4448, 4449, 4450, 4451, 4452, 4454, 4455, 4456, 4457, 4458, 4459, 4460, 4461, 4462, 4463, 4464, 4465, 4466, 4467, 4468, 4470, 4472, 4473, 4474, 4475, 4476, 4477, 4478, 4479, 4480, 4481, 4482, 4483, 4484, 4485, 4486, 4487, 
                                     4488, 4489, 4490, 4492, 4493, 4495, 4496, 4497, 4498, 4499, 4500, 4501, 4502, 4503, 4504, 4505, 4506, 4507, 4508, 4509, 4510, 4511, 
                                     4512, 4513, 4514, 4516, 4517, 4518, 4520, 4521, 4522, 4523, 4524, 4525, 4526, 4438, 4240, 146, 148, 4246, 151, 152, 949, 161, 4258, 171, 4269, 4276, 196, 3336, 1605, 1606, 1607, 1967, 332, 4429, 4430, 4431, 336, 337, 4434, 4435, 4436, 341]
+        
+        self.genders = ["Unisex", "Womens", "Mens"]
+        self.formalities = ["Casual", "Formal"]
+        self.types = ["Top", "Bottom"]
+        self.specific_types = ["Casual","Day Dresses","Cocktail Dresses","Gowns","Skirts","Mini Skirts","Knee Length Skirts","Long Skirts","Tops","Tunics","Blouses","Cardigans","Sweaters","T-Shirts","Outerwear","Coats",
+                    "Jackets","Vests","Jeans","Pants","Shorts","Suits","Swimwear","Activewear","Skinny Jeans","Bootcut Jeans","Wide Leg Jeans","Boyfriend Jeans","Leggings","Jumpsuits","Rompers","Camisoles","Chemises","Pajamas","Robes","Tights","Activewear Tops",
+                    "Activewear Pants","Activewear Skirts","Activewear Shorts","Activewear Jackets","Sports Bras","Clothing","Shirts","Sweaters","T-Shirts","Outerwear","Sportcoats & Blazers","Jeans","Pants","Shorts","Suits",
+                    "Swimwear","Underwear","Sleepwear","Activewear","Activewear Tops","Activewear Pants","Activewear Shorts","Activewear Jackets","Activewear Tank Tops","Straight Leg Jeans","Capri & Cropped Pants","Wedding Dresses","Bikinis",
+                    "One Piece Swimsuits","Cover-ups"]
+        print("len specific types")
+        print(len(self.specific_types))
+
 
     def prep_data(self): # get resnet output and category id one-hot encoding of whole dataset
 
@@ -122,9 +134,9 @@ class Data():
         #should no longer need this
 
         batch = torch.empty(0, 3, 224, 224)
-        categories = torch.empty(0, 380)
-        
-        
+        categories = torch.empty(0, len(self.genders) + len(self.formalities) + len(self.types) + len(self.specific_types))
+
+        garments_printed = 0
         
         for i in data:
             
@@ -135,14 +147,9 @@ class Data():
             self.cum_len += len(i["items"])
             self.outfit_boundaries = torch.cat((self.outfit_boundaries, torch.tensor([self.cum_len])))
 
-            garments_printed = 0
-
             j = 0
             while j < len(i["items"]):
-                if (garments_printed < 20):
-                    print(i["items"][j]["categoryid"])
-                    garments_printed += 1
-                if (i["items"][j]["categoryid"] in self.category_ids):
+                if (str(i["items"][j]["categoryid"]) in self.clothing_dict):
                     img = Image.open(folder + str(i["items"][j]["index"]) + ".jpg")
                     if (img.mode != "RGB"):
                         img = img.convert('RGB')
@@ -154,27 +161,20 @@ class Data():
                     
                     item_id = i["items"][j]["categoryid"]
                     
-                    gender = clothing_dict[str(item_id)]["gender"]
-                    genders = ["Unisex", "Womens", "Mens"]
-                    gender_one_hot = nn.functional.one_hot(torch.as_tensor(self.category_ids.index(i["items"][j]["categoryid"])), genders.indexof(gender)).unsqueeze(0)
+                    gender = self.clothing_dict[str(item_id)]["gender"]
+                    gender_one_hot = nn.functional.one_hot(torch.as_tensor(self.genders.index(gender)), len(self.genders))
                     
-                    formality = clothing_dict[str(item_id)]["formality"]
-                    formalities = ["Casual", "Formal"]
-                    formality_one_hot = nn.functional.one_hot(torch.as_tensor(self.category_ids.index(i["items"][j]["categoryid"])), formalities.indexof(formality)).unsqueeze(0)
+                    formality = self.clothing_dict[str(item_id)]["formality"]
                     
-                    type = clothing_dict[str(item_id)]["type"]
-                    types = ["Top", "Bottom"]
-                    type_one_hot = nn.functional.one_hot(torch.as_tensor(self.category_ids.index(i["items"][j]["categoryid"])), types.indexof(type)).unsqueeze(0)
+                    formality_one_hot = nn.functional.one_hot(torch.as_tensor(self.formalities.index(formality)), len(self.formalities))
                     
-                    specific_type = clothing_dict[str(item_id)]["specific_type"]
-                    specific_types = ["Casual","Day Dresses","Cocktail Dresses","Gowns","Skirts","Mini Skirts","Knee Length Skirts","Long Skirts","Tops","Tunics","Blouses","Cardigans","Sweaters","T-Shirts","Outerwear","Coats",
-                    "Jackets","Vests","Jeans","Pants","Shorts","Suits","Swimwear","Activewear","Skinny Jeans","Bootcut Jeans","Wide Leg Jeans","Boyfriend Jeans","Leggings","Jumpsuits","Rompers","Camisoles","Chemises","Pajamas","Robes","Tights","Activewear Tops",
-                    "Activewear Pants","Activewear Skirts","Activewear Shorts","Activewear Jackets","Sports Bras","Clothing","Shirts","Sweaters","T-Shirts","Outerwear","Sportcoats & Blazers","Jeans","Pants","Shorts","Suits",
-                    "Swimwear","Underwear","Sleepwear","Activewear","Activewear Tops","Activewear Pants","Activewear Shorts","Activewear Jackets","Activewear Tank Tops","Straight Leg Jeans","Capri & Cropped Pants","Wedding Dresses","Bikinis",
-                    "One Piece Swimsuits","Cover-up"]
-                    specific_type_one_hot = nn.functional.one_hot(torch.as_tensor(self.category_ids.index(i["items"][j]["categoryid"])), specific_types.indexof(specific_type)).unsqueeze(0)
+                    type = self.clothing_dict[str(item_id)]["type"]
+                    type_one_hot = nn.functional.one_hot(torch.as_tensor(self.types.index(type)), len(self.types))
                     
-                    one_hot = torch.cat((type_one_hot, formality_one_hot, gender_one_hot, specific_type_one_hot), dim=1)   
+                    specific_type = self.clothing_dict[str(item_id)]["specific_type"]
+                    specific_type_one_hot = nn.functional.one_hot(torch.as_tensor(self.specific_types.index(specific_type)), len(self.specific_types))
+                    
+                    one_hot = torch.cat((gender_one_hot, formality_one_hot, type_one_hot, specific_type_one_hot)).unsqueeze(0)
                     
                     categories = torch.cat((categories, one_hot))
                 j += 1
@@ -185,6 +185,7 @@ class Data():
         batch = torch.cat((batch, categories), 1)
 
         for i in range(20):
+            print("category id one hot encoding")
             print(batch[i][2048:])
 
         return batch
